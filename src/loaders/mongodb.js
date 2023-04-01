5b87e27db665019f0fab0a4986981f2fde1b2244
MauricioAlcantara <mauricio.dashe1@gmail.com>
2023-04-01 02:42:28 -0300

    ajuste no projeto

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.set(`strictQuery`, true)
 
 async function startDB(){
-    await mongoose.connect('mongodb+srv://MauricioAlcantara:Xf27COv85P@cluster0.nfln2lz.mongodb.net/API-Mauricio');
+    await mongoose.connect('sua-url-de-conexao-do-mongodb');
 }
 
 module.exports = startDB;
